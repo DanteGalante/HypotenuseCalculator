8ab6554a30fa7c8ad85fd313ae4a15bb09aef88b
Dan <olveradan@hotmail.com>
2021-10-14 16:45:35 -0500

    Corrections to the input type of the EditText "etResultado" and format of output
    We don't need to write in the EditText component, so I changed its input type.
    
    I also changed the output format of the result, to show only two digits.

--- app/src/main/java/com/uv/hypotenusecalculator/MainActivity.kt
+++ app/src/main/java/com/uv/hypotenusecalculator/MainActivity.kt
@@ -2,9 +2,12 @@ package com.uv.hypotenusecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import java.lang.Math.sqrt
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etCatetoA: EditText
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         etCatetoA = findViewById(R.id.etCatetoA)
         etCatetoB = findViewById(R.id.etCatetoB)
         etResultado = findViewById(R.id.etResultado)
+        etResultado.inputType = InputType.TYPE_NULL
         btnLimpiar = findViewById(R.id.btnLimpiar)
         btnCalcular = findViewById(R.id.btnCalcular)
 
@@ -30,14 +34,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnCalcular?.setOnClickListener{
-            var catetoA_valor:Double = etCatetoA?.text.toString().toDouble();
-            var catetoB_valor:Double = etCatetoB?.text.toString().toDouble();
+            if (validateInputsLenght()) {
+                var catetoA_valor:Double = etCatetoA?.text.toString().toDouble();
+                var catetoB_valor:Double = etCatetoB?.text.toString().toDouble();
 
-            var catetoB = Math.pow(catetoB_valor,2.0)
-            var catetoA = Math.pow(catetoA_valor,2.0)
-            var resultadoxd = sqrt(catetoB + catetoA)
+                var catetoB = Math.pow(catetoB_valor,2.0)
+                var catetoA = Math.pow(catetoA_valor,2.0)
+                var resultado = sqrt(catetoB + catetoA)
 
-            etResultado.setText(resultadoxd.toString())
+                var formatDecimal:DecimalFormat = DecimalFormat("#.00")
+
+                etResultado.setText(formatDecimal.format(resultado).toString())
+            } else {
+                Toast.makeText(applicationContext,"Error: Introduce un valor para ambos catetos",Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    fun validateInputsLenght(): Boolean {
+        if (etCatetoA.length() == 0) {
+            return false;
+        }
+        if (etCatetoB.length() == 0) {
+            return false;
+        }
+        return true;
     }
 }
